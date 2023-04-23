090af16f8d8d59e1463187e1d698dd99c0a8a59b
Fernando López <fernando.lpzg2@gmail.com>
2023-04-23 14:32:48 -0600

    Commented some stuff so the app could work

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,8 +3,8 @@ require 'poke-api-v2'
 class PokemonsController < ApplicationController
   def index
     pokemon = params[:pokemon]
-    response = PokeApi.get(pokemon: { limit: 151 }).results
-    @pokemon = JSON.parse(response.body, symbolize_names: true)
+    @pokemons = PokeApi.get(pokemon: { limit: 151 }).results
+    #@pokemon = JSON.parse(response.body, symbolize_names: true)
   end
   def show
     @pokemon = PokeApi.get(pokemon: params[:id])
