3cb05570aa1a720e8c7d0d4ecae56e082c4a2020
Fernando López <fernando.lpzg2@gmail.com>
2023-04-23 14:11:14 -0600

    Created Article model

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,7 +2,9 @@ require 'poke-api-v2'
 
 class PokemonsController < ApplicationController
   def index
-    @pokemons = PokeApi.get(pokemon: { limit: 151 }).results
+    pokemon = params[:pokemon]
+    response = PokeApi.get(pokemon: { limit: 151 }).results
+    @pokemon = JSON.parse(response.body, symbolize_names: true)
   end
   def show
     @pokemon = PokeApi.get(pokemon: params[:id])
