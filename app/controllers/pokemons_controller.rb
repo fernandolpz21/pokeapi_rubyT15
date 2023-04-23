a86d9432f6cfb7c52ca6a7b090c1be375d8f95c2
Fernando López <fernando.lpzg2@gmail.com>
2023-04-23 15:25:07 -0600

    Insert images attempt

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,9 +2,11 @@ require 'poke-api-v2'
 
 class PokemonsController < ApplicationController
   def index
-    pokemon = params[:pokemon]
     @pokemons = PokeApi.get(pokemon: { limit: 151 }).results
-    #@pokemon = JSON.parse(response.body, symbolize_names: true)
+    @pokemon_sprites = @pokemons.map do |pokemon|
+      pokemon_data = PokeApi.get(pokemon: pokemon.name)
+      pokemon_data.sprites.front_default
+    end
   end
   def show
     @pokemon = PokeApi.get(pokemon: params[:id])
